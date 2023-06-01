c01df9931442e23e7fea2c467aa07229c036f86e
Gabriel <gabri.mevial@gmail.com>
2023-06-01 17:10:19 -0300

    primeiro requisito funcao average

--- src/average.js
+++ src/average.js
@@ -11,8 +11,18 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  
+const average = (array) => {
+  let soma = 0;
+  if (array.length === 0) {
+    return undefined;
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof (array[index]) !== 'number') {
+      return undefined;
+    }
+    soma += array[index];
+  }
+  return Math.round(soma / array.length);
 };
-
+console.log(average([3, 4, 5]));
 module.exports = average;
